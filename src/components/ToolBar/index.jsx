c43543b7ff50f753f4938607cf937f1e667565be
jeedeey <speshies@gmail.com>
2020-10-12 13:01:15 +0100

    commit

--- src/components/ToolBar/index.jsx
+++ src/components/ToolBar/index.jsx
@@ -2,13 +2,13 @@ import React, {Component} from "react";
 import UserLinks from "../UserLinks";
 import config from "../../../data/SiteConfig";
 import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
-import "./ToolBar.scss"
+import toolbarStyles from "./ToolBar.scss"
 
 
 class Toolbar extends Component {
     render() {
       return (
-        <header className="toolbar">
+        <header className={toolbarStyles.toolbar}>
             <nav className="toolbar_nav">
                 <div className="toolbar_logo">
                     <a href="/"><img src={config.siteLogo}/></a>
