2b82886de19cf300e8dcc30d3d62f13a3e82ef06
jeedeey <speshies@gmail.com>
2020-11-01 02:27:14 +0100

    commit

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -15,11 +15,11 @@ class Contact extends Component {
                   <h2>Get In Touch</h2>
                   <h5>Don't hesitate to contact us</h5>
                 </div>
-                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                <form name="contact" method="post" data-netlify="true">
                     <input type="text" name="message-name" className="form-control mb-30" placeholder="Your Name" />
                     <input type="email" name="message-email" class="form-control mb-30" placeholder="Your Email" />
                     <textarea name="message" class="form-control mb-30" placeholder="Your Message"></textarea>
-                    <button>Send Message</button>
+                    <button type="submit" class="btn poca-btn">Send Message</button>
                 </form>
               </div>
               <div className="contact-info">
